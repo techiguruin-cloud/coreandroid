d3f44e0665f795307fafe1b92071540c7cde91d9
techiguruin-cloud <techiguru.in@gmail.com>
2025-09-20 14:13:09 +0530

    constraintlayout theme appname appicon

--- app/src/main/java/com/srmu/androidbasic/MainActivity.java
+++ app/src/main/java/com/srmu/androidbasic/MainActivity.java
@@ -1,6 +1,11 @@
 package com.srmu.androidbasic;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //declare the java fields
+    Button play, stop;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mediaPlayer = MediaPlayer.create(this,
+                Uri.parse("https://utkarshpandey.com/assets/sound/nuclear-siren.mp3"));
+        //to initialize the java fields from UI widgets
+        play = findViewById(R.id.btn_play);
+        stop = findViewById(R.id.btn_stop);
+        //to click on the buttons
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "playing",
+                        Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
     }
 }
